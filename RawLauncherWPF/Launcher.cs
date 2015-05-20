2a76209a5644251e0834736518e49e2be2d6a434
AnakinSklavenwalker <Anakin@gmx-topmail.de>
2015-05-20 18:55:59 +0200

    Moved Startup to a other Class. First concept of the Resource Extractor

---
+++ RawLauncherWPF/Launcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace RawLauncherWPF
+{
+    public class Launcher
+    {
+        [STAThread]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main()
+        {
+            var launcher = new Launcher();
+            launcher.StartLauncher();
+        }
+
+
+        public void StartLauncher()
+        {
+            MessageBox.Show("Test");
+            var main = new App();
+            main.InitializeComponent();
+            main.Run();
+        }
+    }
+}
---
+++ RawLauncherWPF/ResourceExtractor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace RawLauncherWPF
+{
+    internal class ResourceExtractor
+    {
+        private string Assembly { get; }
+
+
+        public ResourceExtractor()
+        {
+            //TODO: set default assembly
+        }
+
+        public ResourceExtractor(string AssemblyName)
+        {
+            Assembly = AssemblyName;
+        }
+
+        /// <summary>
+        /// Extracts internal resources out of the assembly into a directory if the files are not already in it
+        /// </summary>
+        /// <param name="directory">Target directory</param>
+        /// <param name="files">Set of Files </param>
+        public void ExtractFilesIfRequired(string directory, IEnumerable<string> files)
+        {
+        }
+    }
+}
