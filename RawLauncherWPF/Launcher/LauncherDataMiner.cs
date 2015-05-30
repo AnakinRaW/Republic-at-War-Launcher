a97f99ec92ce2b54fe2ff20dfba43e81d5de6516
AnakinSklavenwalker <anakin@gmx-topmail.de>
2015-05-30 14:41:48 +0200

    properties are not static

--- RawLauncherWPF/Launcher/LauncherDataMiner.cs
+++ RawLauncherWPF/Launcher/LauncherDataMiner.cs
@@ -7,43 +7,32 @@ namespace RawLauncherWPF.Launcher
 {
     public class LauncherDataMiner
     {
-        public const string GameconstantsUpdateHashEaW = "1d44b0797c8becbe240adc0098c2302a";
+        public const string EeawForum = "http://www.everythingeaw.com/forum/index.php";
         public const string GameconstantsUpdateHash = "4306d0c45d103cd11ff6743d1c3d9366";
+        public const string GameconstantsUpdateHashEaW = "1d44b0797c8becbe240adc0098c2302a";
         public const string GraphicdetailsUpdateHash = "4d7e140887fc1dd52f47790a6e20b5c5";
+        public const string ModdbPage = "http://www.moddb.com/mods/republic-at-war";
         public const string Server = "";
         public const string ServerUrl = "";
-        public const string EeawForum = "http://www.everythingeaw.com/forum/index.php";
-        public const string ModdbPage = "http://www.moddb.com/mods/republic-at-war";
-
         public static readonly string CurrentDirectory = Directory.GetCurrentDirectory();
-        public static LauncherDataMiner DataMiner { get; private set; }
-
-        public static Game Foc { get; private set; }
-
-        public static Game Eaw { get; private set; }
-        public static Mod CurrentMod { get; private set; }
-
-        public static string UpdateDownloadDir { get; private set; }
-
-        public static string RestoreDownloadDir { get; private set; }
 
         public LauncherDataMiner()
         {
             DataMiner = this;
         }
 
+        public static LauncherDataMiner DataMiner { get; private set; }
+        public Mod CurrentMod { get; private set; }
+        public Game Eaw { get; private set; }
+        public Game Foc { get; private set; }
+        public string RestoreDownloadDir { get; private set; }
+        public string UpdateDownloadDir { get; private set; }
+
         public void SetCurrentMod(Mod mod)
         {
             if (mod == null)
                 throw new NullReferenceException();
             CurrentMod = mod;
-        }
-
-        public void SetFocGame(Game game)
-        {
-            if (game == null)
-                throw new NullReferenceException();
-            Foc = game;
         }
 
         public void SetEawGame(Game game)
@@ -53,13 +42,11 @@ namespace RawLauncherWPF.Launcher
             Eaw = game;
         }
 
-        public void SetUpdateDownloadDir(string path)
+        public void SetFocGame(Game game)
         {
-            if (path == null)
-                throw new NullReferenceException(nameof(path));
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-            UpdateDownloadDir = path;
+            if (game == null)
+                throw new NullReferenceException();
+            Foc = game;
         }
 
         public void SetRestoreDownloadDir(string path)
@@ -69,6 +56,15 @@ namespace RawLauncherWPF.Launcher
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
             RestoreDownloadDir = path;
+        }
+
+        public void SetUpdateDownloadDir(string path)
+        {
+            if (path == null)
+                throw new NullReferenceException(nameof(path));
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            UpdateDownloadDir = path;
         }
     }
 }
