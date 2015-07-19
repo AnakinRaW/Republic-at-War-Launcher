a98c94f94da6e43949a940c38a687af00005cdf1
AnakinSklavenwalker <anakin@gmx-topmail.de>
2015-07-19 16:20:38 +0200

    added some commands

--- RawLauncherWPF/ViewModels/MainWindowViewModel.cs
+++ RawLauncherWPF/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Controls;
 using System.Windows.Media;
+using ModernApplicationFramework.Commands;
 using RawLauncherWPF.UI;
 using MainWindow = ModernApplicationFramework.Controls.MainWindow;
 
@@ -7,7 +8,7 @@ namespace RawLauncherWPF.ViewModels
 {
     public sealed class MainWindowViewModel : ModernApplicationFramework.ViewModels.MainWindowViewModel
     {
-        private MainWindow _mainWindow;
+        private readonly MainWindow _mainWindow;
 
         private ILauncherPane _activePane;
 
@@ -36,5 +37,64 @@ namespace RawLauncherWPF.ViewModels
         //        OnPropertyChanged();
         //    }
         //}
+
+        #region Commands
+
+        public Command OpenModdbCommand => new Command(OpenModdb);
+
+        private void OpenModdb()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command OpenEeawCommand => new Command(OpenEeaw);
+
+        private void OpenEeaw()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command ShowPlayPaneCommand => new Command(ShowPlayPane);
+
+        private void ShowPlayPane()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command ShowCheckPaneCommand => new Command(ShowCheckPane);
+
+        private void ShowCheckPane()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command ShowLanguagePaneCommand => new Command(ShowLanguage);
+
+        private void ShowLanguage()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command ShowRestorePaneCommand => new Command(ShowRestorePane);
+
+        private void ShowRestorePane()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command ShowUpdatePaneCommand => new Command(ShowUpdatePane);
+
+        private void ShowUpdatePane()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Close()
+        {
+            _mainWindow.Close();
+            base.Close();
+        }
+
+        #endregion
     }
 }
