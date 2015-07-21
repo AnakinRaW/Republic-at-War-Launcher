29b2fe11f273429215f237f66797cb365ddce7d4
AnakinSklavenwalker <anakin@gmx-topmail.de>
2015-07-21 16:47:55 +0200

    Added Command Bindings

--- RawLauncherWPF/ViewModels/LanguageViewModel.cs
+++ RawLauncherWPF/ViewModels/LanguageViewModel.cs
@@ -1,16 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using ModernApplicationFramework.Commands;
 using RawLauncherWPF.UI;
+using RawLauncherWPF.Utilities;
 
 namespace RawLauncherWPF.ViewModels
 {
     public sealed class LanguageViewModel : LauncherPaneViewModel
     {
+        private LanguageTypes _selectedLanguage;
         public LanguageViewModel(ILauncherPane pane) : base(pane)
         {
+            _selectedLanguage = LanguageTypes.None;
         }
+
+        #region Commands
+        public Command<object> ChangeSelectionCommand => new Command<object>(ChangeSelection, CanChangeSelection);
+
+        private void ChangeSelection(object obj)
+        {
+            AudioHelper.PlayAudio(AudioHelper.Audio.ButtonPress);
+            _selectedLanguage = (LanguageTypes) obj;
+        }
+
+        private bool CanChangeSelection(object arg)
+        {
+            return true;
+        }
+
+        public Command ChangeLanguageCommand => new Command(ChangeLanguage);
+
+        private void ChangeLanguage()
+        {
+            MessageBox.Show("f");
+        }
+
+        #endregion
     }
+
+    public enum LanguageTypes
+    {
+        None,
+        Dutch,
+        English,
+        French,
+        German,
+        Italian,
+        Russian,
+        Serbian,
+        Spanish,
+        Swedish,
+        Ukrainian
+    }
+
 }
