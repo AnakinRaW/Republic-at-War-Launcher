bce1e67f29f320b194a3662a2d848ded259ddc09
AnakinSklavenwalker <anakin@gmx-topmail.de>
2015-07-22 14:53:24 +0200

    Added Image Utilites

---
+++ RawLauncherWPF/Utilities/ImageUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace RawLauncherWPF.Utilities
+{
+    class ImageUtilities
+    {
+        public static ImageSource GetImageSourceFromPath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                throw new NoNullAllowedException(nameof(path));
+            return new BitmapImage(new Uri(@"pack://application:,,,/RawLauncherWPF;component/" + path));
+        }
+    }
+}
