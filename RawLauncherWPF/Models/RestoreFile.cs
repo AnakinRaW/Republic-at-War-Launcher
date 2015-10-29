a1a8f70650f865dffdd211e29167a99d32adb35f
AnakinSklavenwalker <anakin@gmx-topmail.de>
2015-10-29 12:41:49 +0100

    Added Restore Models

---
+++ RawLauncherWPF/Models/RestoreFile.cs
@@ -0,0 +1,21 @@
+﻿namespace RawLauncherWPF.Models
+{
+    public class RestoreFile
+    {
+        public string Name { get; set; }
+
+        public string TargetPath { get; set; }
+
+        public FileAction Action { get; set; }
+
+        public string SourcePath { get; set; }
+
+        public TargetType TargetType { get; set; }
+    }
+
+    public enum FileAction
+    {
+        Download,
+        Delete
+    }
+}
---
+++ RawLauncherWPF/Models/RestoreTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RawLauncherWPF.Models
+{
+    public class RestoreTable
+    {
+        public RestoreTable()
+        {
+            Files = new List<RestoreFile>();
+        }
+        public Version Version { get; set; }
+
+        public List<RestoreFile> Files;
+
+        public List<RestoreFile> GetFilesOfType(TargetType type)
+        {
+            return Files.Where(files => files.TargetType == type).ToList();
+        } 
+    }
+}
